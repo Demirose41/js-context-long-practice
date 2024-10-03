20e66036c3bfc4c6b4f01a7acd04609a200e5d4c
demirose41 <demirose41@gmail.com>
2024-10-02 21:40:01 -0700

    4: Completed problems 04-07

--- problems/06-fancy-calculator.js
+++ problems/06-fancy-calculator.js
@@ -1,4 +1,29 @@
-// Your code here
+const Calculator = require('./02-calculator');
+
+// module.imports = require('./02-calculator');
+
+class FancyCalculator extends Calculator{
+	constructor(total){
+		super(total);
+	}
+
+	setTotal(newTotal){
+		this.total = newTotal;
+		return this.total;
+	}
+
+	modulo(num){
+		const newTotal = this.total % num;
+		this.setTotal(newTotal);
+		return this.total;
+	}
+
+	squared(){
+		this.setTotal(this.total * this.total);
+		return this.total;
+	}
+
+}
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
